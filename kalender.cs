881ec332ee0f9938bbe63222e1672b927b0e9f36
GettinOver <33653479+GettinOver@users.noreply.github.com>
2018-06-28 08:29:14 +0200

    Add files via upload

--- kalender.cs
+++ kalender.cs
@@ -7,16 +7,19 @@ using System.Threading.Tasks;
 namespace Wiederholung {
     class Program {
         static void Main(string[] args) {
-			int atn = 0, atn2;
+            int atn = 0, atn2, urTag, zielTag;
 			bool gueltig;
 			string termin;
+			string[] termine = new string[32];
 			string[] days = new string[32];
 			string[] menu = { "Wählen Sie die gewünschte Operation aus:", "(1) Kalender aufrufen", "(2) Termin hinzufügen", "(3) Termin löschen", "(4) Termin verschieben", "(5) Programm beenden" };
 
+			//array days füllen (für ausgabe von kalender)
 			for (int i = 1; i < days.Length; i++) {
 				days[i] = "Tag " + i + ": ";
 			}
 
+			//ausgabe des arrays menu (zur ausgabe des menüs)
 			do {
 				foreach (string operation in menu) {
 					Console.WriteLine(operation);
@@ -31,14 +34,16 @@ namespace Wiederholung {
 				Console.Clear();
 			} while (!gueltig);
 
+			//verarbeitung des menüs
 			switch (atn) {
-				case 1:
+				case 1://kalender aufrufen
 					foreach (string day in days) {
 						Console.WriteLine(day);
 					}
 
 					Console.WriteLine("");
 
+					//ausgabe des arrays menu (zur ausgabe des menüs)
 					do {
 						foreach (string operation in menu) {
 							Console.WriteLine(operation);
@@ -48,7 +53,7 @@ namespace Wiederholung {
 						if (!gueltig) {
 							Console.Clear();
 							Console.WriteLine("Bitte geben Sie eine Zahl zwischen 1 und 5 ein");
-							Console.ReadLine();
+							Console.ReadKey();
 						}
 						Console.Clear();
 					} while (!gueltig);
@@ -65,7 +70,7 @@ namespace Wiederholung {
 						goto case 5;
 					}
 					break;
-				case 2:
+				case 2://termin hinzufügen
 					do {
 						Console.Clear();
 						Console.WriteLine("An welchem Tag wollen Sie einen Termin hinzufügen?");
@@ -75,7 +80,7 @@ namespace Wiederholung {
 						if (!gueltig) {
 							Console.Clear();
 							Console.WriteLine("Bitte geben Sie eine Zahl zwischen 1 und 31 ein");
-							Console.ReadLine();
+							Console.ReadKey();
 						}
 					} while (!gueltig);
 
@@ -83,11 +88,19 @@ namespace Wiederholung {
 					Console.WriteLine("Bitte bennen Sie Ihren Termin");
 					termin = Convert.ToString(Console.ReadLine());
 
+					//eintrag des termins in das array termine
 					for (int i = atn2; i == atn2; i++) {
-						days[i] = days[i] + termin;
+						termine[i] = termin;
+					}
+
+					//hinzufügen des termins ins array days
+					for (int i = atn2; i == atn2; i++) {
+						days[i] = days[i] + termine[i];
 					}
 
 					Console.Clear();
+
+					//ausgabe des arrays menu (zur ausgabe des menüs)
 					do {
 						foreach (string operation in menu) {
 							Console.WriteLine(operation);
@@ -97,7 +110,7 @@ namespace Wiederholung {
 						if (!gueltig) {
 							Console.Clear();
 							Console.WriteLine("Bitte geben Sie eine Zahl zwischen 1 und 5 ein");
-							Console.ReadLine();
+							Console.ReadKey();
 						}
 						Console.Clear();
 					} while (!gueltig);
@@ -114,7 +127,7 @@ namespace Wiederholung {
 						goto case 5;
 					}
 					break;
-				case 3:
+				case 3://termin löschen
 					do {
 						Console.Clear();
 						Console.WriteLine("An welchem Tag wollen Sie einen Termin entfernen?");
@@ -124,14 +137,16 @@ namespace Wiederholung {
 						if (!gueltig) {
 							Console.Clear();
 							Console.WriteLine("Bitte geben Sie eine Zahl zwischen 1 und 31 ein");
-							Console.ReadLine();
+							Console.ReadKey();
 						}
 					} while (!gueltig);
 
+					//überschreiben des eingegebenen tages mit default wert
 					for (int i = atn2; i == atn2; i++) {
 						days[i] = "Tag " + i + ": ";
 					}
 
+					//ausgabe des arrays menu (zur ausgabe des menüs)
 					do {
 						foreach (string operation in menu) {
 							Console.WriteLine(operation);
@@ -141,7 +156,7 @@ namespace Wiederholung {
 						if (!gueltig) {
 							Console.Clear();
 							Console.WriteLine("Bitte geben Sie eine Zahl zwischen 1 und 5 ein");
-							Console.ReadLine();
+							Console.ReadKey();
 						}
 						Console.Clear();
 					} while (!gueltig);
@@ -158,11 +173,41 @@ namespace Wiederholung {
 						goto case 5;
 					}
 					break;
-				case 4:
+				case 4://termin verschieben
 					do {
-						Console.WriteLine("Diese Funktion muss noch implementiert werden. Bitte wählen Sie eine andere Operation aus");
-						Console.WriteLine("");
+						Console.Clear();
+						Console.WriteLine("Bitte geben Sie den Ursprungstag ein");
+						gueltig = int.TryParse(Console.ReadLine(), out urTag);
 
+						if (!gueltig) {
+							Console.Clear();
+							Console.WriteLine("Bitte geben Sie eine Zahl zwischen 1 und 31 ein");
+							Console.ReadKey();
+						}
+					} while (!gueltig);
+
+					do {
+						Console.Clear();
+						Console.WriteLine("Bitte geben Sie den Zieltag ein");
+						gueltig = int.TryParse(Console.ReadLine(), out zielTag);
+
+						if (!gueltig) {
+							Console.Clear();
+							Console.WriteLine("Bitte geben Sie eine Zahl zwischen 1 und 31 ein");
+							Console.ReadKey();
+						}
+						Console.Clear();
+					} while (!gueltig);
+
+					//verschieben des termins
+					for (int i = zielTag; i == zielTag ; i++) {
+						days[i] = days[i] + termine[urTag];     //speichern des termines im zieltag
+						for (int n = urTag; n == urTag; n++) {
+							days[n] = "Tag " + n + ": ";		//ersetzen des ursprungstages durch default wert
+						}
+					}
+
+					do {
 						foreach (string operation in menu) {
 							Console.WriteLine(operation);
 						}
@@ -171,7 +216,7 @@ namespace Wiederholung {
 						if (!gueltig) {
 							Console.Clear();
 							Console.WriteLine("Bitte geben Sie eine Zahl zwischen 1 und 5 ein");
-							Console.ReadLine();
+							Console.ReadKey();
 						}
 						Console.Clear();
 					} while (!gueltig);
@@ -188,13 +233,12 @@ namespace Wiederholung {
 						goto case 5;
 					}
 					break;
-				case 5:
+				case 5://console schließen
 					Environment.Exit(0);
 					break;
 			}
 
-			Console.ReadLine();
+			Console.ReadKey();
 		}
-
     }
 }
